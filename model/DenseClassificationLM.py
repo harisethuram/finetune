8da2da0a15b662519f7e5bfdd9ca4edb8a5ae7b1
littleghostrei <rei.tao@outlook.com>
2024-05-30 13:46:07 -0700

    Alternative Classification Head Model First Implementation

---
+++ model/DenseClassificationLM.py
@@ -0,0 +1,50 @@
+import sys
+
+unwanted_path = '/mmfs1/home/hsethu/.local/lib/python3.9/site-packages'
+if unwanted_path in sys.path:
+    sys.path.remove(unwanted_path)
+
+
+from transformers import AutoTokenizer, AutoModelForCausalLM#, DataCollatorWithPadding, HfArgumentParser, BitsAndBytesConfig
+from datasets import load_dataset, Dataset
+from sklearn.metrics import f1_score, accuracy_score
+from tqdm import tqdm
+import os
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+import numpy as np
+
+class DenseClassificationLM(nn.Module):
+    def __init__(self, pretrained_model, head, num_labels, freeze_layers=False):
+        super(DenseClassificationLM, self).__init__()
+        self.emb_layer = pretrained_model.get_input_embeddings()
+        self.pretrained_model = pretrained_model
+        self.head = head
+        if freeze_layers:
+            for param in self.pretrained_model.parameters():
+                param.required_grad = False
+
+    def forward(self, input_ids, attention_mask, want_embs=False):
+        input_embs = self.emb_layer(input_ids)
+        input_embs.requires_grad_()
+        input_embs.retain_grad()
+        outputs = self.pretrained_model(inputs_embeds=input_embs, attention_mask=attention_mask, output_hidden_states=True)
+        # outputs = self.pretrained_model(input_ids, attention_mask, output_hidden_states=True)
+
+        last_layer = outputs.hidden_state[-1]
+        seq_len = last_layer.shape[1]
+
+        sum_logits = 0
+        for i in range(seq_len):
+            sum_logits += self.head(outputs.hidden_state[-1][:, i, ...])
+
+        mean_logits = sum_logits / seq_len
+
+        if want_embs:
+            return mean_logits, input_embs
+        return mean_logits
---
+++ utils/evaluate_dense.py
@@ -0,0 +1,38 @@
+import sys
+
+unwanted_path = '/mmfs1/home/hsethu/.local/lib/python3.9/site-packages'
+if unwanted_path in sys.path:
+    sys.path.remove(unwanted_path)
+
+from transformers import AutoTokenizer, AutoModelForCausalLM#, DataCollatorWithPadding, HfArgumentParser, BitsAndBytesConfig
+from datasets import load_dataset, Dataset
+from sklearn.metrics import f1_score, accuracy_score
+from tqdm import tqdm
+import os
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+import numpy as np
+
+def evaluate(model, loader, num_labels, classification_idx=-1, cap=1000):
+    model.eval()
+    y_hat = []
+    y = []
+    with torch.no_grad():
+        for batch in loader:
+            if len(y) > cap:
+                break
+            if classification_idx is None:
+                logits = model(batch[0], batch[1], want_embs=False)
+            else:
+                logits = model(batch[0], batch[1], classification_idx=classification_idx, want_embs=False)
+            y_hat += logits.argmax(dim=-1).tolist()
+            y += batch[2].tolist()
+    avg = "binary"
+    if num_labels > 2:
+        avg = "macro"
+    return f1_score(y, y_hat, average=avg), accuracy_score(y, y_hat), y_hat
